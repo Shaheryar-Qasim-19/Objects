ca9d62ec2f7443160c4aa5707194eec221288a4b
Shaheryar Qasim <57341412+Shaheryar-Qasim-19@users.noreply.github.com>
2021-07-01 09:25:41 +0500

    Object assignment completed
    object

---
+++ app.js
@@ -0,0 +1,56 @@
+// **********QUESTION 1**********
+
+// var items = [
+//     {
+//         name: "Juice",
+//         price: 50,
+//         quantity: "3",
+//     },
+//     {
+//         name: "Cookie",
+//         price: 30,
+//         quantity: "9",
+//     },
+//     {
+//         name: "Shirt",
+//         price: 400,
+//         quantity: "20",
+//     }
+// ]
+
+// console.log(items[0].price + items[1].price + items[2].price)
+// console.log(items[0].price * items[0].quantity + items[1].price * items[1].quantity + items[2].price * items[2].quantity)
+
+
+// **********QUESTION 2**********
+
+// var property = {
+//     name: "Shaheryar Qasim",
+//     email: "shaheryar.qasim@gmail.com",
+//     password: "12345",
+//     age: "20",
+//     gender: "Male",
+//     city: "Karachi",
+//     country: "Pakistan",
+// }
+
+// console.log("age" in property)
+// console.log("country" in property)
+// console.log("firstName" in property)
+// console.log("lastName" in property)
+
+
+// **********QUESTION 3**********
+
+// function Student(name,id,gender,password){
+//     this.name = name;
+//     this.id = id;
+//     this.gender = gender;
+//     this.password = password;
+// }
+
+// var plan1 = new Student("Sherry","420","Male","78945")
+// var plan2 = new Student("Ahsan","3652","Male","65412")
+
+// console.log(plan1)
+// console.log(plan2)
